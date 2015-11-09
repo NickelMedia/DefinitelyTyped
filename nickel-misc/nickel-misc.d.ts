992b7156eb3f047451a25ef655f7261fea551ac7
Greg Webber <greg@jnickel.com>
2015-11-09 14:07:05 -0400

    added typings for google-maps so we can load google maps async

--- nickel-misc/nickel-misc.d.ts
+++ nickel-misc/nickel-misc.d.ts
@@ -4,3 +4,14 @@ declare module "Object-watch-polyfill" {}
 declare module "history.js" {}
 declare module "gsap" {}
 declare var TEMPLATE_DATA:any;
+declare module "google-maps" {
+    var KEY:string;
+    var CLIENT:string;
+    var VERSION:string;
+    var SENSOR:boolean;
+    var LIBRARIES:string[];
+    var LANGUAGE:string;
+    function load():void;
+    function release():void;
+    function onLoad():void;
+}
